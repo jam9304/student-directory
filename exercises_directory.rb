5647831f3e5cf126fb40e9d623cb237b8dde49bb
Mooney <jmooney9304@gmail.com>
2022-02-20 22:50:49 +0000

    Loading students method added

--- exercises_directory.rb
+++ exercises_directory.rb
@@ -67,6 +67,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit" 
 end
 
@@ -84,6 +85,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -106,6 +109,15 @@ def save_students
     student_data = [student[:name], student[:cohort], student[:origin_country], student[:hobby]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort, country, hobby = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, origin_country: country, hobby: hobby}
   end
   file.close
 end
