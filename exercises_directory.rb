31152036dac70a09b23d0d436e1b02cc1d75cc3f
Mooney <jmooney9304@gmail.com>
2022-02-20 20:58:38 +0000

    Interative Menu refractored

--- exercises_directory.rb
+++ exercises_directory.rb
@@ -1,3 +1,5 @@
+@students = [] #global variable
+
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
@@ -24,7 +26,7 @@ def input_students
     puts "And finally, what country were they born in?"
     country = gets.strip
     #  add the student hash to the array
-    students << {name: name, cohort: cohort, hobby: hobby, origin_country: country}
+    @students << {name: name, cohort: cohort, hobby: hobby, origin_country: country}
     #get another name from the user
     puts "Thank you, now the next student please."
     name = gets.strip
@@ -41,29 +43,56 @@ def print_header
   puts output_2.center(100)
 end
 
-def print(students)
-    students.each_with_index do |student, number|  
+def print_students_list
+    @students.each_with_index do |student, number|  
     output_3 = "#{number + 1}. #{student[:name]} (#{student[:cohort]} cohort). They were born in #{student[:origin_country]} and enjoys #{student[:hobby]}" 
     puts output_3.center(100)
   end
 end
 
 
-def print_footer(students)
-  puts " "
-  if students.count < 2
-  output_4 = "Overall, we have #{students.count} great student"
+def print_footer
+  puts " " #just to get a line split the end result
+  if @students.count < 2
+  output_4 = "Overall, we have #{@students.count} great student"
   puts output_4.center(100)
   else
-  output_5 = "Overall, we have #{students.count} great students"
+  output_5 = "Overall, we have #{@students.count} great students"
   puts output_5.center(100)
   end
 end
 
-
-students = input_students
-print_header
-if students.length > 0
-  print(students)
-  print_footer(students)
+def print_menu
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" 
 end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
