82612eab2b3c8d7f85878827d0004da564196829
Mooney <jmooney9304@gmail.com>
2022-02-20 14:09:27 +0000

    Add students manually has been added

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-#The array of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #  add the student hash to the array
+    students << {name: name, cohort: :november}
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return array of students
+  students
+end
+
 #Methods for the header, student students and footer
 def print_header
   puts "The students of Villians Academy"
@@ -28,6 +31,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
